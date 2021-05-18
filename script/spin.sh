d5460897650f6eb82109bee2d3b1dc38589680b9
Alex Watt <alex.watt@shopify.com>
2021-05-18 13:35:18 -0400

    Set --extensions-dir for Spin

--- script/spin.sh
+++ script/spin.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 git clone git@github.com:Shopify/dev-test-runner /src/github.com/shopify/dev-test-runner
-/usr/bin/code --install-extension "$(find /src/github.com/shopify/dev-test-runner/*.vsix | tail -1)"
+EXTENSIONS_DIR=/home/${USER}/.vscode-server/extensions
+cp "$(find /src/github.com/shopify/dev-test-runner/*.vsix | tail -1)" "${EXTENSIONS_DIR}/dev-test-runner.vsix"
+/usr/bin/code --extensions-dir="${EXTENSIONS_DIR}" --install-extension "${EXTENSIONS_DIR}/dev-test-runner.vsix"
