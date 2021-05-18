0d7cad9bfc3c5c7bd9911d27d632616609ea6f10
Alex Watt <alex.watt@shopify.com>
2021-05-18 10:19:14 -0400

    Use /bin/code-server to install extension on Spin

--- script/spin.sh
+++ script/spin.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git clone git@github.com:Shopify/dev-test-runner /src/github.com/shopify/dev-test-runner
-code --install-extension "$(find /src/github.com/shopify/dev-test-runner/*.vsix | tail -1)"
+/bin/code-server --install-extension "$(find /src/github.com/shopify/dev-test-runner/*.vsix | tail -1)"
