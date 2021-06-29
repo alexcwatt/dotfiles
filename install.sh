0a96c2c5d54bf875367eb7587fa49f7fc1129a57
Alex Watt <alex@alexcwatt.com>
2021-06-29 08:20:35 -0400

    Add ohmyzsh config

--- install.sh
+++ install.sh
@@ -19,3 +19,5 @@ if [ -d $DOTFILES_DIR ] && [ ! -L $DOTFILES_DIR ]; then
 else
   echo "Could not find dotfiles directory: $DOTFILES_DIR"
 fi
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
