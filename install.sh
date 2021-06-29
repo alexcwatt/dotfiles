14292a911519208eed093fe491803ca7657bab9b
Alex Watt <alex@alexcwatt.com>
2021-06-29 08:09:56 -0400

    Install dotfiles with install.sh, and add git aliases

--- install.sh
+++ install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
 if [ "$SPIN" ]; then
+  DOTFILES_DIR=~/dotfiles
+
   ./script/spin.sh
+else
+  DOTFILES_DIR=~/repos-personal/dotfiles
+fi
+
+if [ -d $DOTFILES_DIR ] && [ ! -L $DOTFILES_DIR ]; then
+  for file in "$DOTFILES_DIR"/dotfiles/*; do
+    echo "Installing $file..."
+    f=".$(basename "$file")"
+    target=~/"$f"
+    [ -f "$target" ] && mv "$target" "$target.bak"
+    ln -s "$file"  ~/"$f"
+  done
+else
+  echo "Could not find dotfiles directory: $DOTFILES_DIR"
 fi
