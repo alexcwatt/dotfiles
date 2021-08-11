d31e2186175c73c24dc8f71d6e8c0191ddab7002
Alex Watt <alex@alexcwatt.com>
2021-08-11 08:57:47 -0400

    Setup beta flags in Spin

--- script/spin.sh
+++ script/spin.sh
@@ -4,3 +4,6 @@ git clone git@github.com:Shopify/dev-test-runner /src/github.com/shopify/dev-tes
 EXTENSIONS_DIR=/home/${USER}/.vscode-server/extensions
 cp "$(find /src/github.com/shopify/dev-test-runner/*.vsix | tail -1)" "${EXTENSIONS_DIR}/dev-test-runner.vsix"
 /usr/bin/code --extensions-dir="${EXTENSIONS_DIR}" --install-extension "${EXTENSIONS_DIR}/dev-test-runner.vsix"
+
+# Setup beta flags
+~/data/default/beta_flags.sh
