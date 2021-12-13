4a33c9ec155078bb08b4a9bd4ec7db6bc1571631
Alex Watt <alex@alexcwatt.com>
2021-12-12 20:56:17 -0500

    Add more customization to Mac setup

--- script/mac.sh
+++ script/mac.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 (cd ~ || exit; brew bundle)
