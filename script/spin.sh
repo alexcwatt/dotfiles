b89440188a5721edb56e869cd7e9c5f974fe570c
Alex Watt <alex.watt@shopify.com>
2022-01-24 09:02:07 -0500

    Remove dev-test-runner setup
    Now installed in repos directly

--- script/spin.sh
+++ script/spin.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-git clone git@github.com:Shopify/dev-test-runner /src/github.com/shopify/dev-test-runner
-EXTENSIONS_DIR=/home/${USER}/.vscode-server/extensions
-cp "$(find /src/github.com/shopify/dev-test-runner/*.vsix | tail -1)" "${EXTENSIONS_DIR}/dev-test-runner.vsix"
-/usr/bin/code --extensions-dir="${EXTENSIONS_DIR}" --install-extension "${EXTENSIONS_DIR}/dev-test-runner.vsix"
-
 # Setup beta flags
 ~/data/default/beta_flags.sh
