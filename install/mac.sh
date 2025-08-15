891eefa09fd3640c7dda6566a3e31e153fe53a14
Alex Watt <alex@alexcwatt.com>
2025-08-14 23:00:59 -0400

    Always save screencaptures in ~/Documents/Screenshots

--- install/mac.sh
+++ install/mac.sh
@@ -13,6 +13,10 @@ defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
+# Create Screenshots directory and configure screenshot location
+mkdir -p ~/Documents/Screenshots
+defaults write com.apple.screencapture location ~/Documents/Screenshots
+
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 if [[ -d /opt/dev ]]; then
